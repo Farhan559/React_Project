eb70a4890b51725e4ee3ab6b3777b6f36bd605f7
Farhan559 <farhansiddiqui559@gmail.com>
2024-11-19 15:18:40 +0500

    fix: changes resolved

--- src/components/My routes/Contact.jsx
+++ src/components/My routes/Contact.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Contact = () => {
-  return (
+    const navigate = useNavigate();
+    return (
     <>
       <h2>This is Contact Page</h2>
+      <button onClick={()=>navigate('/home')}>Go to Home</button>
+      <button onClick={()=>navigate('/about')}>Go to AboutUS</button>
     </>
   )
 }
